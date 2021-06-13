82393e2fd575a82bf0ce39b71262df9492b32ea0
Test <Test@abc.com>
2021-06-13 18:35:57 +1000

    Initial Commit

---
+++ CRUD/CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDApp.swift
+//  CRUD
+//
+//  Created by David on 13/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
