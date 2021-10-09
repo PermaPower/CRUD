294583c544e09b8cb8082ecc94dd6b17bb88343c
David <dp@abc.com>
2021-10-09 19:07:13 +1100

    Crease added

--- CRUD/CRUDApp.swift
+++ CRUD/CRUDApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct CRUDApp: App {
     var body: some Scene {
         WindowGroup {
-          ContentView()
+          //ContentView()
+            TagShapeView()
           /// Flip()
           //  ImagePickerView()
         }
--- CRUD/TagShapeView.swift
+++ CRUD/TagShapeView.swift
@@ -12,25 +12,39 @@ struct TagShapeView: View {
         
         ScrollView() {
             GeometryReader { (geometry) in
+                ZStack(){
+ 
                 VStack(spacing: 0) {
                    
                     TagTop()
                         .frame(width: geometry.size.width-40, height: 80)
-                        .foregroundColor(Color.red)
-                    
+                        .foregroundColor(Color.orange)
+            
                     RoundedRectangle(cornerRadius: 5)
                         .fill(Color.orange)
                         .frame(width: .infinity, height: 350)
                     
                     TagBottom()
                         .foregroundColor(Color.orange)
-                        .frame(width: geometry.size.width-80, height: 150)
+                        .frame(width: geometry.size.width-80, height: 250)
+                    
                 }
-            }
             .compositingGroup()
             .shadow(color: Color.primary, radius: 10, x: 0, y: 0)
             .padding(.init(top: 0, leading: 10, bottom: 10, trailing: 10))
-        }
+            
+            crease()
+                .stroke()
+                .foregroundColor(Color.red)
+                .shadow(color: .blue, radius: 5, x: 5, y: 5)
+                    
+            crease1()
+                 .stroke()
+                 .foregroundColor(Color.blue)
+                 .shadow(color: .blue, radius: 5, x: -5, y: 5)
+        
+                }
+            }
         .frame(minWidth: 0, maxWidth: .infinity)
     }
 }
@@ -64,4 +78,28 @@ struct TagShapeView_Previews: PreviewProvider {
     static var previews: some View {
         TagShapeView()
     }
+}
+
+struct crease: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX-80, y: rect.midY+100))
+        path.addLine(to: CGPoint(x: rect.midX-10, y: rect.maxY-50))
+
+        return path
+    }
+ }
+    
+    struct crease1: Shape {
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+            path.move(to: CGPoint(x: rect.midX-40, y: rect.midY+150))
+            path.addLine(to: CGPoint(x: rect.midX-10, y: rect.maxY-80))
+
+            return path
+        }
+     }
+    
 }
